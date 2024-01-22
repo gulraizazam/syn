eeaf62ef72236b5ce8f0b27a38717dd674c21010
gulraizazam <gulraizazam0@gmail.com>
2024-01-23 01:17:52 +0500

    Resolved

--- app/User.php
+++ app/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,24 @@ Route::get('/', function () {
     return view('admin.home');
 });
 
-Route::get('login',[UserController::class,'login'])->name('loginform');
-Route::post('login',[UserController::class,'postLogin'])->name('postlogin');
-Route::get('logout',[UserController::class,'logout'])->name('logout');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('login','UserController@login')->name('loginform');
+Route::post('login','UserController@postLogin')->name('postlogin');
+Route::get('logout','UserController@logout')->name('logout');
+Route::get('/home', 'HomeController@index')->name('home');
 
 /////////students////
-Route::get('students',[StudentController::class,'index'])->name('students');
-Route::get('students/add',[StudentController::class,'add'])->name('students.add');
-Route::post('students/add',[StudentController::class,'store'])->name('students.store');
-Route::get('students/edit/{id}',[StudentController::class,'edit'])->name('students.edit');
-Route::post('students/update/{id}',[StudentController::class,'update'])->name('students.update');
-Route::get('students/delete/{id}',[StudentController::class,'delete'])->name('students.delete');
+Route::get('students','StudentController@index')->name('students');
+Route::get('students/add','StudentController@add')->name('students.add');
+Route::post('students/add','StudentController@store')->name('students.store');
+Route::get('students/edit/{id}','StudentController@edit')->name('students.edit');
+Route::post('students/update/{id}','StudentController@update')->name('students.update');
+Route::get('students/delete/{id}','StudentController@delete')->name('students.delete');
 
 
 /////////////////councellor
-Route::get('councellors',[TeacherController::class,'index'])->name('councellors');
-Route::get('councellors/add',[TeacherController::class,'add'])->name('councellors.add');
-Route::post('councellors/add',[TeacherController::class,'store'])->name('councellors.store');
-Route::get('councellors/edit/{id}',[TeacherController::class,'edit'])->name('councellors.edit');
-Route::post('councellors/update/{id}',[TeacherController::class,'update'])->name('councellors.update');
-Route::get('councellors/delete/{id}',[TeacherController::class,'delete'])->name('councellors.delete');
+Route::get('councellors','TeacherController@index')->name('councellors');
+Route::get('councellors/add','TeacherController@add')->name('councellors.add');
+Route::post('councellors/add','TeacherController@store')->name('councellors.store');
+Route::get('councellors/edit/{id}','TeacherController@edit')->name('councellors.edit');
+Route::post('councellors/update/{id}','TeacherController@update')->name('councellors.update');
+Route::get('councellors/delete/{id}','TeacherController@delete')->name('councellors.delete');
